fb31d0e27830a1d688e26be940776d0227abbefd
Americano5139 <kimjemomaeng@gmail.com>
2025-06-23 08:31:52 +0000

    terrform

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source       = "./modules/vpc"
+  region       = var.region
+  cluster_name = var.cluster_name
+}
+
+module "eks" {
+  source       = "./modules/eks"
+  region       = var.region
+  cluster_name = var.cluster_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.private_subnets
+}
---
+++ modules/eks/main.tf
@@ -0,0 +1,26 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.3"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.30"
+  subnet_ids      = var.subnet_ids
+  vpc_id          = var.vpc_id
+
+  enable_cluster_log_types = []
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size    = 2
+      max_size        = 3
+      min_size        = 1
+      instance_types  = ["t3.medium"]
+      capacity_type   = "ON_DEMAND"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
