ac77d435ed4d0a11debb95cce49c143f18261363
Americano5139 <kimjemomaeng@gmail.com>
2025-06-24 06:57:46 +0000

    terraform

--- variables.tf
+++ variables.tf
@@ -1,7 +1,10 @@
-variable "region" {
-  default = "ap-northeast-2"
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-northeast-2"
 }
 
 variable "cluster_name" {
   default = "eks-cluster"
 }
+
