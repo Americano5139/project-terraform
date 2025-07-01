44929a53bc31e5ab802cc2e7bfc615795fe91ac1
Americano5139 <kimjemomaeng@gmail.com>
2025-07-01 14:38:01 +0900

    Update main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,1 +1,16 @@
+resource "aws_db_instance" "this" {
+  identifier             = "nextcloud-rds"
+  engine                 = "mysql"
+  engine_version         = "8.0"
+  instance_class         = "db.t3.micro"
+  allocated_storage      = 20
+  db_name                = "nextclouddb"
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = var.db_subnet_group_name
+  vpc_security_group_ids = var.db_security_group_ids
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  deletion_protection    = false
+}
 
