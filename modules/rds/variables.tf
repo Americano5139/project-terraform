f3d3805afbbe52af0214096ffdb7de103978fe30
Americano5139 <kimjemomaeng@gmail.com>
2025-07-01 14:38:19 +0900

    Update variables.tf

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,1 +1,21 @@
+variable "db_username" {
+  description = "Master username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_subnet_group_name" {
+  description = "Subnet group name for the RDS instance"
+  type        = string
+}
+
+variable "db_security_group_ids" {
+  description = "List of security group IDs to assign to the RDS instance"
+  type        = list(string)
+}
 
