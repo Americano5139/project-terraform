a48033d8094e8bc46f404b2a3aea5b076b131d18
Americano5139 <kimjemomaeng@gmail.com>
2025-07-01 23:37:00 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,14 @@ variable "cluster_name" {
   default = "eks-cluster"
 }
 
+variable "db_username" {
+  description = "Master username for the RDS instance"
+  type        = string
+}
+
+variable "db_password" {
+  description = "Master password for the RDS instance"
+  type        = string
+  sensitive   = true
+}
+
