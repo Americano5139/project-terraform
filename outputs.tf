3a9b9e8ca21936e9a880a3d5cd58accbca232a2d
Americano5139 <kimjemomaeng@gmail.com>
2025-07-01 12:21:11 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
   value = module.vpc.vpc_id
 }
 
-output "eks_cluster_name" {
-  value = module.eks.cluster_name
-}
+# output "eks_cluster_name" {
+#  value = module.eks.cluster_name
+# }
