196b04166d3981a26447a16bb380d4388bc9cd1f
Americano5139 <kimjemomaeng@gmail.com>
2025-07-02 15:17:05 +0900

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "private_subnets" {
   value = module.vpc.private_subnets
 }
+
+output "default_sg_id" {
+  value = module.vpc.default_security_group_id
+}
+
+output "db_subnet_group_name" {
+  value = module.vpc.database_subnet_group
+}
